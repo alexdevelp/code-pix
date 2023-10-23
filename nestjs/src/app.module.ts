8289aeb5223e6d984c5848b6f762d4dad4a28c3d
Alexandre Pereira <alexandre@placecode.com.br>
2023-10-23 09:51:58 -0300

    adding pix-keys and bank-accounts module

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccount } from './bank-accounts/entities/bank-account.entity';
+import { PixKeysModule } from './pix-keys/pix-keys.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      database: 'nest',
+      username: 'postgres',
+      password: 'root',
+      entities: [BankAccount],
+      synchronize: true,
+    }),
+    BankAccountsModule,
+    PixKeysModule,
+  ],
 })
 export class AppModule {}
